d11a99598d4eff0fbc7851a66ce25089e5cf9157
gnemek20 <gnemek20@gmail.com>
2024-03-07 17:34:13 +0900

    page3 배경화면 설정

--- pages/components/page3.tsx
+++ pages/components/page3.tsx
@@ -1,9 +1,28 @@
 import styles from "@/styles/Page3.module.scss";
+import Image from "next/image";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 const page3 = () => {
+  type imageType = StaticImport;
+  interface imageProps {
+    src: imageType,
+    alt: string
+  }
+
+  const backgrounrdImage: imageProps = {
+    src: require('@/public/images/page3Background.jpg'),
+    alt: 'backgroundImage'
+  }
+
   return (
-    <div>
-      <h1>Page3</h1>
+    <div className={styles.section}>
+      <div className={styles.dimmed}></div>
+      <Image
+        className={styles.backgroundImage}
+        src={backgrounrdImage.src}
+        alt={backgrounrdImage.alt}
+      ></Image>
+      <div className={styles.gradation}></div>
     </div>
   )
 }
